5e1cd4eb0b053140aaaa92e28f4c309f9a631ece
kimdobie <kimdobie@gmail.com>
2022-11-01 16:08:05 -0500

    Initialize Examples-version-1.x

---
+++ utils/addVersionToReadMe.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const fs = require('fs');
+
+const packageJson = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../package.json'))
+);
+
+const { version } = packageJson;
+const readMe = fs.readFileSync(path.join(__dirname, '../README.md'));
+
+let newReadMe = readMe.toString().split('\n');
+while (newReadMe[newReadMe.length - 1].trim() === '') {
+  newReadMe.pop();
+}
+
+newReadMe[newReadMe.length - 1] = `Based on template version ${version}`;
+
+newReadMe = newReadMe.join('\n') + '\n';
+
+fs.writeFileSync(path.join(__dirname, '../README.md'), newReadMe);
+
+process.exitCode = 0;
